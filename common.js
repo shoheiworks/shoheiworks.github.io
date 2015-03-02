e4dd6a6b56601e202814b73e64a577e352508297
Shohei <shoheiworks@gmail.com>
2015-03-02 15:20:25 +0900

    add Img files

--- common.js
+++ common.js
@@ -1,3 +1,15 @@
 window.onload = function(){
-	
+changeImg();
+//End
 }
+
+//ConpBox Img Change
+function changeImg(){
+	$(function(){
+		$("#worksWeb01Img").css("display","none");
+		$("#worksWeb02Img").css("display","none");
+		$("#worksWeb03Img").css("display","none");
+	});
+	$("a")
+};
+
