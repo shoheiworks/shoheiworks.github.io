01d66db7481aee8d7ab09eb3c7544dc8864f09f1
Shohei <shoheiworks@gmail.com>
2015-03-02 19:41:34 +0900

    デザインを整えたJSの設置

--- common.js
+++ common.js
@@ -1,15 +1,57 @@
 window.onload = function(){
-changeImg();
+	showOpenBox();
+	setTimeout("showCompBox();", 3000);
+	changeImg();
 //End
 }
+
+function showOpenBox(){
+    $(function(){
+        $("#openBox").fadeIn(2000);
+    });
+};
+function showCompBox(){
+    $(function(){
+        $("#openBox").fadeOut(1600, function(){
+        	$("#compBox").fadeIn(2600);
+        });
+    });
+};
 
 //ConpBox Img Change
 function changeImg(){
 	$(function(){
 		$("#worksWeb01Img").css("display","none");
 		$("#worksWeb02Img").css("display","none");
+		$("#worksWeb03Img").css("display","block");
+	});
+	$("#worksWeb01Link").hover( function() {
+		$("#worksWeb01Img").css("display","block");
+		$("#worksWeb02Img").css("display","none");
+		$("#worksWeb03Img").css("display","none");		
+	});
+	$("#worksWeb02Link").hover( function() {
+		$("#worksWeb01Img").css("display","none");
+		$("#worksWeb02Img").css("display","block");
 		$("#worksWeb03Img").css("display","none");
 	});
-	$("a")
+	$("#worksWeb03Link").hover( function() {
+		$("#worksWeb01Img").css("display","none");
+		$("#worksWeb02Img").css("display","none");
+		$("#worksWeb03Img").css("display","block");
+	});
+
+	$(function(){
+		$(".appImg02").css("display","block");
+		$(".appImg01").css("display","none");				
+	});
+	$("#worksApp02Link").hover( function() {
+		$(".appImg02").css("display","block");
+		$(".appImg01").css("display","none");
+	});
+	$("#worksApp01Link").hover( function() {
+		$(".appImg02").css("display","none");
+		$(".appImg01").css("display","block");
+	});
 };
 
